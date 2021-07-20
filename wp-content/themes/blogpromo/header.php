906e9b5e2605174cc075e84207c8782507303f66
pierre01470 <gonzales.pierre01470@gmail.com>
2021-07-20 11:27:21 +0200

    header OK

--- wp-content/themes/blogpromo/header.php
+++ wp-content/themes/blogpromo/header.php
@@ -1,23 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() .'/css/style.css'; ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/css/style.css'; ?>">
     <title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?></title>
 </head>
+
 <body>
-    <nav>
-        <ul>
-            <li>
-                <a href="">Accueil</a>
-            </li>
-            <li>
-                <a href="">La Promo</a>
-            </li>
-            <li>
-                <a href="">Les Technos</a>
-            </li>
-        </ul>
-    </nav>
+    <header>
+        <?php wp_nav_menu(); ?>
+    </header>
