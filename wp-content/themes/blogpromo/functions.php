3b5757eb50a28d1c868632e8aa6bad1f5a53848e
pierre01470 <gonzales.pierre01470@gmail.com>
2021-07-21 08:53:06 +0200

    add cpt

--- wp-content/themes/blogpromo/functions.php
+++ wp-content/themes/blogpromo/functions.php
@@ -2,6 +2,14 @@
 add_theme_support('post-thumbnails');
 add_theme_support('menus');
 add_theme_support('title-tag');
+add_theme_support(
+  'post-formats',
+  array(
+    'link',
+    'gallery',
+    'image',
+  )
+);
 
 if (function_exists('add_theme_support')) {
   add_theme_support('post-thumbnails');
@@ -21,7 +29,7 @@ function register_student_post_types()
   // CPT Apprenant
   $labels = array(
     'name' => 'Apprenants',
-    'all_items' => 'Tous les apprenants', // affiché dans le sous menu
+    'all_items' => 'Tous les apprenants',
     'view_item' => 'Voir les apprenants',
     'singular_name' => 'Apprenant',
     'add_new_item' => 'Ajouter un apprenant',
@@ -37,9 +45,38 @@ function register_student_post_types()
     'supports' => array('title', 'editor', 'thumbnail'),
     'taxonomies' => array('category', 'post_tag'),
     'menu_position' => 5,
-    'menu_icon' => 'dashicons-admin-customizer',
+    'menu_icon' => 'dashicons-admin-users',
   );
 
   register_post_type('apprenant', $args);
 }
 add_action('init', 'register_student_post_types'); // Le hook init lance la fonction
+
+function register_presentation_post_types()
+{
+
+  // CPT Presentations
+  $labels = array(
+    'name' => 'Presentation',
+    'all_items' => 'Toutes les presentations', // affiché dans le sous menu
+    'view_item' => 'Voir les presentations',
+    'singular_name' => 'Presentation',
+    'add_new_item' => 'Ajouter une presentation',
+    'edit_item' => 'Modifier une presentation',
+    'menu_name' => 'Presentation'
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'show_in_rest' => true,
+    'has_archive' => true,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'taxonomies' => array('category', 'post_tag'),
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-admin-page',
+  );
+
+  register_post_type('presentation', $args);
+}
+add_action('init', 'register_presentation_post_types'); // Le hook init lance la fonction
