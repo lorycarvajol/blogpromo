098dd3405c7f6a59c03a2e85e4b3a9f7e7834eb9
pierre01470 <gonzales.pierre01470@gmail.com>
2021-07-21 11:55:10 +0200

    start css accueil

--- wp-content/themes/blogpromo/front-page.php
+++ wp-content/themes/blogpromo/front-page.php
@@ -1,19 +1,20 @@
 <?php get_header(); ?>
 <main>
     <section class="banner">
-        <h1>Promo 49</h1>
+        <h1><?php bloginfo('name'); ?></h1>
     </section>
-    <h1><?php bloginfo('name'); ?></h1>
-    <?php while (have_posts()) :  the_post(); ?>
-        <article>
-            <a href="<?php the_permalink(); ?>">
-                <?php the_post_thumbnail('medium'); ?>
-                <h2><?php the_title(); ?></h2>
-            </a>
-            <?php the_excerpt(); ?>
-            <a href="<?php the_permalink(); ?>">
-                Lire
-        </article>
-    <?php endwhile; ?>
+    <section class="last-articles">
+        <h2>Les derniers articles</h2>
+        <?php while (have_posts()) :  the_post(); ?>
+            <article class="accueil-articles">
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail('medium'); ?>
+                    <h2><?php the_title(); ?></h2>
+                </a>
+                <?php the_excerpt(); ?>
+                <a href="<?php the_permalink(); ?>">Lire la suite...</a>
+            </article>
+        <?php endwhile; ?>
+    </section>
 </main>
 <?php get_footer(); ?>
