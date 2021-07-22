f7c99c8ec11716670cacb1b37a02c3c7b50186d4
Carvajol lory <66648964+maclory73@users.noreply.github.com>
2021-07-22 16:21:39 +0200

    page single participant mise en place

--- wp-content/themes/blogpromo/single-apprenant.php
+++ wp-content/themes/blogpromo/single-apprenant.php
@@ -7,7 +7,27 @@
         </div>
         <div class="details">
             <div class="content-card">
-                <h2>dfdsfdsfsdf<br><span>dfgsfgdfgf</span></h2>
+                <h2>dfdsfdsfsdf</h2>
+                <div class="descri-card">
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit nostrum quis cumque suscipit magni praesentium nobis corporis pariatur nam recusandae hic aut, voluptas aliquam reiciendis officiis inventore iure mollitia et?
+                        Blanditiis libero commodi sunt. Porro voluptatem veritatis voluptatum eos soluta accusamus, quia, eum magni labore quo repellendus earum quaerat officiis voluptas libero ad? Ipsum autem labore corporis quidem natus obcaecati?
+                        Neque unde modi explicabo doloribus labore totam nam, saepe officiis nihil molestiae fugit reiciendis aperiam provident vitae vel asperiores magnam assumenda. Architecto tenetur doloribus modi aliquam molestiae dolor id enim!
+         
+
+                    </p>
+                </div>
+                <div class="proj-card">
+                <a class="projet" href="#">
+                        <p>sdfgfg</p>
+                    </a>
+                    <a class="projet" href="#">
+                        <p>fdgdfg</p>
+                    </a>
+                    <a class="projet" href="#">
+                        <p>fgdgfdgdg</p>
+                    </a>
+                </div>
                 <div class="social-icons">
                     <a href="">
                         <img src="<?php echo get_template_directory_uri(); ?>./assets/images/photoshop.png" width="30px" height="30px" alt="" />
@@ -15,12 +35,7 @@
                     <a href="">
                         <img src="<?php echo get_template_directory_uri(); ?>./assets/images/photoshop.png" width="30px" height="30px" alt="" />
                     </a>
-                    <a href="">
-                        <img src="<?php echo get_template_directory_uri(); ?>./assets/images/photoshop.png" width="30px" height="30px" alt="" />
-                    </a>
-                    <a href="">
-                        <img src="<?php echo get_template_directory_uri(); ?>./assets/images/photoshop.png" width="30px" height="30px" alt="" />
-                    </a>
+                   
                 </div>
             </div>
         </div>
