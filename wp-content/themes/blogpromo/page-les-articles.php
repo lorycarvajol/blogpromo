ecca798707456c5c2287d994b4dad3a4b141828a
Carvajol lory <66648964+maclory73@users.noreply.github.com>
2021-07-24 13:40:16 +0200

    voilà

--- wp-content/themes/blogpromo/page-les-articles.php
+++ wp-content/themes/blogpromo/page-les-articles.php
@@ -1,18 +1,33 @@
 <?php get_header(); ?>
-<?php
-$args = array(
-    'post_type' => 'post',
-    'orderby' => 'date',
-    'order' => 'DESC'
-);
-$wp_query = new WP_Query($args);
-while ($wp_query->have_posts()) :
-    $wp_query->the_post(); ?>
-    <h1> <?php the_title(); ?> </h1>
-    <p> <?php the_excerpt(); ?> </p>
-    <a href="<?php the_permalink(); ?>">Lire la suite...</a>
-    <?php endwhile;
-wp_reset_query();
-    ?>
 
-    <?php get_footer(); ?>
+<div class="page-article">
+    <h1>Les articles de la promo 49</h1>
+    <div class="article-search">
+
+    </div>
+    <section class="section-articles">
+        <h2>Les derniers articles</h2>
+        <div class="container-articles">
+            <?php while (have_posts()) :  the_post(); ?>
+                <article class="accueil-articles">
+                    <div class="article">
+                        <?php the_post_thumbnail('medium'); ?>
+
+                        <div class="description">
+                            <h2><?php the_title(); ?></h2>
+                            <p><?php the_excerpt(); ?></p>
+                        </div>
+                        <div class="desc-btn">
+                            <a href="<?php the_permalink(); ?>"><img src="<?php echo get_template_directory_uri(); ?>./assets/Arrow.png" width="20px" height="30px" alt="" /></a>
+                        </div>
+                    </div>
+                </article>
+            <?php endwhile; ?>
+        </div>
+    </section>
+
+</div>
+
+
+
+
