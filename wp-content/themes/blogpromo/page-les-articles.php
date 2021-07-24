4546f262b990ed3ac472271460d44f5a36077000
Carvajol lory <66648964+maclory73@users.noreply.github.com>
2021-07-24 14:43:34 +0200

    input submit mis en place

--- wp-content/themes/blogpromo/page-les-articles.php
+++ wp-content/themes/blogpromo/page-les-articles.php
@@ -3,7 +3,14 @@
 <div class="page-article">
     <h1>Les articles de la promo 49</h1>
     <div class="article-search">
-
+        <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
+            
+            <input type="image" src="<?php echo get_template_directory_uri(); ?>./assets/loupe.png" class="search-submit" value="<?php echo esc_attr_x('Search', 'submit button') ?>" />
+                
+                <input type="search" class="search-field" placeholder="" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label') ?>" />
+            
+           
+        </form>
     </div>
     <section class="section-articles">
         <h2>Les derniers articles</h2>
@@ -27,7 +34,3 @@
     </section>
 
 </div>
-
-
-
-
