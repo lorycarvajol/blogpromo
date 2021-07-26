dd751f840ee1de106998607118f323e7811be679
pierre01470 <gonzales.pierre01470@gmail.com>
2021-07-26 09:47:17 +0200

    articles ok

--- wp-content/themes/blogpromo/page-les-articles.php
+++ wp-content/themes/blogpromo/page-les-articles.php
@@ -4,18 +4,27 @@
     <h1>Les articles de la promo 49</h1>
     <div class="article-search">
         <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
-            
+
             <input type="image" src="<?php echo get_template_directory_uri(); ?>./assets/loupe.png" class="search-submit" value="<?php echo esc_attr_x('Search', 'submit button') ?>" />
-                
-                <input type="search" class="search-field" placeholder="Rechercher un article..." value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label') ?>" />
-            
-           
+
+            <input type="search" class="search-field" placeholder="Rechercher un article..." value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x('Search for:', 'label') ?>" />
+
+
         </form>
     </div>
     <section class="section-articles">
         <h2>Les derniers articles</h2>
         <div class="container-articles">
-            <?php while (have_posts()) :  the_post(); ?>
+            <?php
+            $args = array(
+                'post_type' => 'post',
+                'orderby' => 'date',
+                'order' => 'DESC'
+            );
+            $wp_query = new WP_Query($args);
+            while ($wp_query->have_posts()) :
+                $wp_query->the_post();
+            ?>
                 <article class="accueil-articles">
                     <div class="article">
                         <?php the_post_thumbnail('medium'); ?>
