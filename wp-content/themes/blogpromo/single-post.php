ffac947a99d3ae290592cf43e0411a3289320398
Carvajol lory <66648964+maclory73@users.noreply.github.com>
2021-07-30 15:34:46 +0200

    on affine

--- wp-content/themes/blogpromo/single-post.php
+++ wp-content/themes/blogpromo/single-post.php
@@ -1,19 +1,20 @@
 <?php get_header(); ?>
 <div class="singpost-container">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    
             <article class="post">
-
-                <h1><?php the_title(); ?></h1>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <h1 class="title-post"><?php the_title(); ?></h1>
                 <div class="post__meta">
                     <div class="blocdesc">
                         <p>Par <?php the_author(); ?></p>
                     </div>
-
                     <div class="blocdesc">
-                        <p>Publié le :<?php the_date(); ?></p>
+                        <p>Publié le : <?php the_date(); ?></p>
                     </div>
                     <div class="blocdesc">
-                        <p>Dans la catégorie :<?php the_category(); ?></p>
+                        <p>Dans la catégorie : <?php $categories = get_the_category();
+                                                $category = $categories[0]->cat_name;
+                                                echo $category; ?></p>
                     </div>
                 </div>
                 <div class="blocPost">
@@ -21,30 +22,11 @@
                     <div class="post__content">
                         <?php the_content(); ?>
                     </div>
-                    <?php the_meta(); ?> 
+                    <?php the_meta(); ?>
                 </div>
-               
-                
-<p>
-    <strong>Avis :</strong> 
-    <?php echo get_post_meta( get_the_ID(), 'avis', true ); ?>
-</p>
-
-<p>
-    <strong>Note :</strong>
-    <?php echo get_post_meta( get_the_ID(), 'note', true ); ?> / 10
-</p>
-
-<div class="plus-moins">
-    <div class="plus">
-        <?php echo get_post_meta( get_the_ID(), 'plus', true ); ?>
-    </div>
-    <div class="plus">
-        <?php echo get_post_meta( get_the_ID(), 'moins', true ); ?>
-    </div>
-</div>
-            </article>
-    <?php endwhile;
+                <?php endwhile;
     endif; ?>
+            </article>
+   
 </div>
-<?php get_footer(); ?>
+<p class="single-post-footer"><?php get_footer(); ?></p>
