e10671c15150f8daad8ea2cc79c528cf007e1ece
Carvajol lory <66648964+maclory73@users.noreply.github.com>
2021-08-17 16:31:06 +0200

    ébauche du pluggin

--- wp-content/plugins/socialMedia/socialMedia.php
+++ wp-content/plugins/socialMedia/socialMedia.php
@@ -4,6 +4,16 @@
  */
 
 
- function socialMedia (){
-     
- }
+
+ 
+ add_action('admin_bar_menu', 'socialMedia', 0);
+ function socialMedia(){
+    global $wp_admin_bar;
+    $wp_admin_bar->add_menu( array(
+        'parent' => 'wp-logo',
+        'id' => 'socialMedia',
+        'title' => __('SocialMedia', 'socialMedia'),
+        'href' => '#',
+        'meta' => array('class' => 'socialMedia')
+    ));
+}
