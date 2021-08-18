7e536ba54894be1535a7157963253a175d7abdbe
Carvajol lory <66648964+maclory73@users.noreply.github.com>
2021-08-18 10:08:30 +0200

    plug

--- wp-content/plugins/socialMedia/socialMedia.php
+++ wp-content/plugins/socialMedia/socialMedia.php
@@ -12,108 +12,111 @@
 
 
  
-add_action( 'widgets_init' , 'socialMedia_init' );
+// créer widget qui permet de récuper l'url de medias sociaux et générer le shortcode pour aficher l'icone  
+class SocialMedia_Widget extends WP_Widget {
 
-function socialMedia_init(){
-
-   register_widget("widgetSocialMedia");
-}
-
-class socialMedia extends WP_Widget {
-
-   function socialMedia() {
-
-      $widget_ops = array('classname' => 'socialMedia', 'description' => 'Affiche les icones des médias sociaux');
-      $this->WP_Widget('socialMedia', 'SocialMedia', $widget_ops);
-   }
-
-   function widget($args, $instance) {
-
-      extract($args);
-      $title = apply_filters('widget_title', $instance['title']);
-      $media = $instance['media'];
-      $media_url = $instance['media_url'];
-      $media_url_img = $instance['media_url_img'];
-      $media_url_img_2 = $instance['media_url_img_2'];
-      $media_url_img_3 = $instance['media_url_img_3'];
-      $media_url_img_4 = $instance['media_url_img_4'];
+   // constructeur
+   function SocialMedia_Widget() {
+      // on définit le nom et l'identifiant du widget
+      $this->widget_name = 'SocialMedia';
+      $this->widget_id = 'SocialMedia';
+      // on définit la description du widget
+      $this->widget_description = 'Widget permettant d\'afficher les icones de médias sociaux';
+      // on définit le nom de la classe de l'widget
+      $this->widget_class = 'SocialMedia_Widget';
+      // on définit le nom de la classe CSS de l'widget
+      $this->widget_cssclass = 'socialMedia_widget';
+      // on définit le nom de l'attribut id du widget
+      $this->widget_attribute_id = 'socialMedia_widget';
+      // on définit le nom de la classe CSS de l'affichage du widget
+      $this->widget_cssclass_instance = 'socialMedia_widget_instance';
+      
+      $this->widget_cssclass_title = 'socialMedia_widget_title';
      
+      $this->widget_cssclass_content = 'socialMedia_widget_content';
+      
+      $this->widget_cssclass_footer = 'socialMedia_widget_footer';
+      
+      $this->widget_cssclass_footer_content = 'socialMedia_widget_footer_content';
+      
+      $this->widget_cssclass_footer_title = 'socialMedia_widget_footer_title';
+    
+      $this->widget_cssclass_footer_icon = 'socialMedia_widget_footer_icon';
+      
+      $this->widget_cssclass_footer_icon_facebook = 'socialMedia_widget_footer_icon_facebook';
+     
+      $this->widget_cssclass_footer_icon_twitter = 'socialMedia_widget_footer_icon_twitter';
+      
+      $this->widget_cssclass_footer_icon_google = 'socialMedia_widget_footer_icon_google';
+      
+      $this->widget_cssclass_footer_icon_linkedin = 'socialMedia_widget_footer_icon_linkedin';
+      
+      $this->widget_cssclass_footer_icon_youtube = 'socialMedia_widget_footer_icon_youtube';
+      
+      $this->widget_cssclass_footer_icon_instagram = 'socialMedia_widget_footer_icon_instagram';
+    
 
-      echo $before_widget;
-      if ( $title )
-         echo $before_title . $title . $after_title;
-      ?>
-      <div class="media-wrapper">
-         <?php if($media_url_img != ''){ ?>
-            <a href="<?php echo $media_url_img; ?>" class="media-image" target="_blank"><img src="<?php echo $media_url_img; ?>" alt="<?php echo $media_url_img; ?>" /></a>
-
-         }
-         if($media_url_img_2 != ''){ ?>
-            <a href="<?php echo $media_url_img_2; ?>" class="media-image" target="_blank"><img src="<?php echo $media_url_img_2; ?>" alt="<?php echo $media_url_img_2; ?>" /></a>
-         }
-         if($media_url_img_3 != ''){ ?>
-            <a href="<?php echo $media_url_img_3; ?>" class="media-image" target="_blank"><img src="<?php echo $media_url_img_3; ?>" alt="<?php echo $media_url_img_3; ?>" /></a>
-         }
-         if($media_url_img_4 != ''){ ?>
-            <a href="<?php echo $media_url_img_4; ?>" class="media-image" target="_blank"><img src="<?php echo $media_url_img_4; ?>" alt="<?php echo $media_url_img_4; ?>" /></a>
-         }
-         ?>
-      </div>
-      <?php
-      echo $after_widget;
    }
 
-   function update($new_instance, $old_instance) {
-      $instance = $old_instance;
-      $instance['title'] = strip_tags($new_instance['title']);
-      $instance['media'] = strip_tags($new_instance['media']);
-      $instance['media_url'] = strip_tags($new_instance['media_url']);
-      $instance['media_url_img'] = strip_tags($new_instance['media_url_img']);
-      $instance['media_url_img_2'] = strip_tags($new_instance['media_url_img_2']);
-      $instance['media_url_img_3'] = strip_tags($new_instance['media_url_img_3']);
-      $instance['media_url_img_4'] = strip_tags($new_instance['media_url_img_4']);
+   // on définit le nom de la classe CSS de l'affichage du widget
+   function widget($args, $instance) {
+      // on définit le titre du widget
+      $title = apply_filters('widget_title', $instance['title']);
+      // on définit le contenu du widget
+      $content = apply_filters('widget_content', $instance['content']);
+      // on définit le footer du widget
+      $footer = apply_filters('widget_footer', $instance['footer']);
+     
+      $footer_content = apply_filters('widget_footer_content', $instance['footer_content']);
+      
+      $footer_title = apply_filters('widget_footer_title', $instance['footer_title']);
+      
+      $footer_icon = apply_filters('widget_footer_icon', $instance['footer_icon']);
+      
+      $footer_icon_facebook = apply_filters('widget_footer_icon_facebook', $instance['footer_icon_facebook']);
+   
+      $footer_icon_twitter = apply_filters('widget_footer_icon_twitter', $instance['footer_icon_twitter']);
+      
+      $footer_icon_google = apply_filters('widget_footer_icon_google', $instance['footer_icon_google']);
+      
+      $footer_icon_linkedin = apply_filters('widget_footer_icon_linkedin', $instance['footer_icon_linkedin']);
+      
+      $footer_icon_youtube = apply_filters('widget_footer_icon_youtube', $instance['footer_icon_youtube']);
+      
+      $footer_icon_instagram = apply_filters('widget_footer_icon_instagram', $instance['footer_icon_instagram']);
 
-      return $instance;
-   }
+      echo $args['before_widget'];
+      if ($title) {
+         echo $args['before_title'] . $title . $args['after_title'];
+      }
+      echo '<div class="socialMedia_widget_container">';
+      echo '<div class="socialMedia_widget_content">';
+      echo $content;
+      echo '</div>';
+      echo '<div class="socialMedia_widget_footer">';
+      echo '<div class="socialMedia_widget_footer_content">';
+      echo $footer_content;
+      echo '</div>';
+      echo '<div class="socialMedia_widget_footer_title">';
+      echo $footer_title;
+      echo '</div>';
+      echo '<div class="socialMedia_widget_footer_icon">';
+      echo '<a href="'.$footer_icon_facebook.'" target="_blank"><i class="fa fa-facebook"></i></a>';
+      echo '<a href="'.$footer_icon_twitter.'" target="_blank"><i class="fa fa-twitter"></i></a>';
+      echo '<a href="'.$footer_icon_google.'" target="_blank"><i class="fa fa-google-plus"></i></a>';
+      echo '<a href="'.$footer_icon_linkedin.'" target="_blank"><i class="fa fa-linkedin"></i></a>';
+      echo '<a href="'.$footer_icon_youtube.'" target="_blank"><i class="fa fa-youtube-play"></i></a>';
+      echo '<a href="'.$footer_icon_instagram.'" target="_blank"><i class="fa fa-instagram"></i></a>';
 
-   function form($instance) {
-      $defaults = array('title' => '', 'media' => '', 'media_url' => '', 'media_url_img' => '', 'media_url_img_2' => '', 'media_url_img_3' => '', 'media_url_img_4' => '');
-      $instance = wp_parse_args((array) $instance, $defaults); ?>
-      <p>
-         <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'tbare'); ?></label>
-         <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" />
-      </p>
-      <p>
-         <label for="<?php echo $this->get_field_id('media'); ?>"><?php _e('Media:', 'tbare'); ?></label>
-         <select id="<?php echo $this->get_field_id('media'); ?>" name="<?php echo $this->get_field_name('media'); ?>" class="widefat">
-            <option value="">Select Media</option>
-            <option value="facebook" <?php if($instance['media'] == 'facebook') echo 'selected="selected"'; ?>>Facebook</option>
-            <option value="twitter" <?php if($instance['media'] == 'twitter') echo 'selected="selected"'; ?>>Twitter</option>
-
-         </select>
-      </p>
-      <p>
-         <label for="<?php echo $this->get_field_id('media_url'); ?>"><?php _e('Media URL:', 'tbare'); ?></label>
-         <input class="widefat" id="<?php echo $this->get_field_id('media_url'); ?>" name="<?php echo $this->get_field_name('media_url'); ?>" type="text" value="<?php echo $instance['media_url']; ?>" />
-      </p>
-      <p>
-         <label for="<?php echo $this->get_field_id('media_url_img'); ?>"><?php _e('Media URL Image:', 'tbare'); ?></label>
-         <input class="widefat" id="<?php echo $this->get_field_id('media_url_img'); ?>" name="<?php echo $this->get_field_name('media_url_img'); ?>" type="text" value="<?php echo $instance['media_url_img']; ?>" />
-      </p>
-      <p>
-         <label for="<?php echo $this->get_field_id('media_url_img_2'); ?>"><?php _e('Media URL Image 2:', 'tbare'); ?></label>
-         <input class="widefat" id="<?php echo $this->get_field_id('media_url_img_2'); ?>" name="<?php echo $this->get_field_name('media_url_img_2'); ?>" type="text" value="<?php echo $instance['media_url_img_2']; ?>" />
-      </p>
-      <p>
-         <label for="<?php echo $this->get_field_id('media_url_img_3'); ?>"><?php _e('Media URL Image 3:', 'tbare'); ?></label>
-         <input class="widefat" id="<?php echo $this->get_field_id('media_url_img_3'); ?>" name="<?php echo $this->get_field_name('media_url_img_3'); ?>" type="text" value="<?php echo $instance['media_url_img_3']; ?>" />
-      </p>
-      <p>
-         <label for="<?php echo $this->get_field_id('media_url_img_4'); ?>"><?php _e('Media URL Image 4:', 'tbare'); ?></label>
-         <input class="widefat" id="<?php echo $this->get_field_id('media_url_img_4'); ?>" name="<?php echo $this->get_field_name('media_url_img_4'); ?>" type="text" value="<?php echo $instance['media_url_img_4']; ?>" />
-      </p>
-   <?php
+      echo '</div>';
+      echo '</div>';
+      echo $args['after_widget'];
    }
 }
 
+
+// on définit le nom de la classe CSS de l'affichage du widget
+function socialMedia_register_widgets() {
+   register_widget('socialMedia_widgets');
+}
 
