3b01a1e3e64f49a5901ee37fe21944b689528336
Carvajol lory <66648964+maclory73@users.noreply.github.com>
2021-08-23 08:43:25 +0200

    mise en route du lundi

--- wp-content/plugins/socialMedia/socialMedia.php
+++ wp-content/plugins/socialMedia/socialMedia.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: SocialMedia
+ * Plugin Name: réseaux sociaux
  * * Plugin URI: 
  * Description: plugin pour ajouté icone média sociaux depuis l'admin board
  * Version: 0.1
@@ -9,114 +9,89 @@
  * Author URI:
  */
 
+//lier css et js
+function socialmedia_enqueue_style() {
+    wp_enqueue_style('my_custom_style', plugin_dir_url( __FILE__) . 'custom/style.css');
+    }
+    add_action('admin_print_styles', 'socialmedia_enqueue_style');
 
-
- 
-// créer widget qui permet de récuper l'url de medias sociaux et générer le shortcode pour aficher l'icone  
-class SocialMedia_Widget extends WP_Widget {
-
-   // constructeur
-   function SocialMedia_Widget() {
-      // on définit le nom et l'identifiant du widget
-      $this->widget_name = 'SocialMedia';
-      $this->widget_id = 'SocialMedia';
-      // on définit la description du widget
-      $this->widget_description = 'Widget permettant d\'afficher les icones de médias sociaux';
-      // on définit le nom de la classe de l'widget
-      $this->widget_class = 'SocialMedia_Widget';
-      // on définit le nom de la classe CSS de l'widget
-      $this->widget_cssclass = 'socialMedia_widget';
-      // on définit le nom de l'attribut id du widget
-      $this->widget_attribute_id = 'socialMedia_widget';
-      // on définit le nom de la classe CSS de l'affichage du widget
-      $this->widget_cssclass_instance = 'socialMedia_widget_instance';
-      
-      $this->widget_cssclass_title = 'socialMedia_widget_title';
-     
-      $this->widget_cssclass_content = 'socialMedia_widget_content';
-      
-      $this->widget_cssclass_footer = 'socialMedia_widget_footer';
-      
-      $this->widget_cssclass_footer_content = 'socialMedia_widget_footer_content';
-      
-      $this->widget_cssclass_footer_title = 'socialMedia_widget_footer_title';
-    
-      $this->widget_cssclass_footer_icon = 'socialMedia_widget_footer_icon';
-      
-      $this->widget_cssclass_footer_icon_facebook = 'socialMedia_widget_footer_icon_facebook';
-     
-      $this->widget_cssclass_footer_icon_twitter = 'socialMedia_widget_footer_icon_twitter';
-      
-      $this->widget_cssclass_footer_icon_google = 'socialMedia_widget_footer_icon_google';
-      
-      $this->widget_cssclass_footer_icon_linkedin = 'socialMedia_widget_footer_icon_linkedin';
-      
-      $this->widget_cssclass_footer_icon_youtube = 'socialMedia_widget_footer_icon_youtube';
-      
-      $this->widget_cssclass_footer_icon_instagram = 'socialMedia_widget_footer_icon_instagram';
-    
-
-   }
-
-   // on définit le nom de la classe CSS de l'affichage du widget
-   function widget($args, $instance) {
-      // on définit le titre du widget
-      $title = apply_filters('widget_title', $instance['title']);
-      // on définit le contenu du widget
-      $content = apply_filters('widget_content', $instance['content']);
-      // on définit le footer du widget
-      $footer = apply_filters('widget_footer', $instance['footer']);
-     
-      $footer_content = apply_filters('widget_footer_content', $instance['footer_content']);
-      
-      $footer_title = apply_filters('widget_footer_title', $instance['footer_title']);
-      
-      $footer_icon = apply_filters('widget_footer_icon', $instance['footer_icon']);
-      
-      $footer_icon_facebook = apply_filters('widget_footer_icon_facebook', $instance['footer_icon_facebook']);
-   
-      $footer_icon_twitter = apply_filters('widget_footer_icon_twitter', $instance['footer_icon_twitter']);
-      
-      $footer_icon_google = apply_filters('widget_footer_icon_google', $instance['footer_icon_google']);
-      
-      $footer_icon_linkedin = apply_filters('widget_footer_icon_linkedin', $instance['footer_icon_linkedin']);
-      
-      $footer_icon_youtube = apply_filters('widget_footer_icon_youtube', $instance['footer_icon_youtube']);
-      
-      $footer_icon_instagram = apply_filters('widget_footer_icon_instagram', $instance['footer_icon_instagram']);
-
-      echo $args['before_widget'];
-      if ($title) {
-         echo $args['before_title'] . $title . $args['after_title'];
-      }
-      echo '<div class="socialMedia_widget_container">';
-      echo '<div class="socialMedia_widget_content">';
-      echo $content;
-      echo '</div>';
-      echo '<div class="socialMedia_widget_footer">';
-      echo '<div class="socialMedia_widget_footer_content">';
-      echo $footer_content;
-      echo '</div>';
-      echo '<div class="socialMedia_widget_footer_title">';
-      echo $footer_title;
-      echo '</div>';
-      echo '<div class="socialMedia_widget_footer_icon">';
-      echo '<a href="'.$footer_icon_facebook.'" target="_blank"><i class="fa fa-facebook"></i></a>';
-      echo '<a href="'.$footer_icon_twitter.'" target="_blank"><i class="fa fa-twitter"></i></a>';
-      echo '<a href="'.$footer_icon_google.'" target="_blank"><i class="fa fa-google-plus"></i></a>';
-      echo '<a href="'.$footer_icon_linkedin.'" target="_blank"><i class="fa fa-linkedin"></i></a>';
-      echo '<a href="'.$footer_icon_youtube.'" target="_blank"><i class="fa fa-youtube-play"></i></a>';
-      echo '<a href="'.$footer_icon_instagram.'" target="_blank"><i class="fa fa-instagram"></i></a>';
-
-      echo '</div>';
-      echo '</div>';
-      echo $args['after_widget'];
-   }
+//création  widget affichant les icones de réseaux sociaux  dans le footer
+function socialMedia_widget() {
+    register_widget( 'socialMedia_widget' );
 }
+add_action( 'widgets_init', 'socialMedia_widget' );
 
 
-// on définit le nom de la classe CSS de l'affichage du widget
-function socialMedia_register_widgets() {
-   register_widget('socialMedia_widgets');
+class socialmedia_widget extends WP_Widget {
+
+    function __construct() {
+        parent::__construct(
+            'socialmedia_widget', // Base ID
+            __( 'réseaux sociaux', 'text_domain' ), // Name
+            array( 'description' => __( 'Widget pour afficher les icones de réseaux sociaux', 'text_domain' ), ) // Args
+        );
+    }
+
+    public function widget( $args, $instance ) {
+        echo $args['before_widget'];
+        if ( ! empty( $instance['title'] ) ) {
+            echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+        }
+        echo '<div class="socialmedia">';
+        echo '<a href="'.$instance['facebook'].'"><img src="'.plugins_url( 'media/facebook.png', __FILE__ ).'"class="iconesoc" alt="facebook"></a>';
+        echo '<a href="'.$instance['twitter'].'"><img src="'.plugins_url( 'media/twitter.png', __FILE__ ).'"class="iconesoc" alt="twitter"></a>';
+        echo '<a href="'.$instance['instagram'].'"><img src="'.plugins_url( 'media/instagram.png', __FILE__ ).'"class="iconesoc" alt="instagram"></a>';
+        echo '<a href="'.$instance['linkedin'].'"><img src="'.plugins_url( 'media/linkedin.png', __FILE__ ).'"class="iconesoc" alt="linkedin"></a>';
+        echo '</div>';
+        echo $args['after_widget'];
+    }
+
+    public function form( $instance ) {
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'text_domain' );
+        $facebook = ! empty( $instance['facebook'] ) ? $instance['facebook'] : __( '', 'text_domain' );
+        $twitter = ! empty( $instance['twitter'] ) ? $instance['twitter'] : __( '', 'text_domain' );
+        $instagram = ! empty( $instance['instagram'] ) ? $instance['instagram'] : __( '', 'text_domain' );
+        $linkedin = ! empty( $instance['linkedin'] ) ? $instance['linkedin'] : __( '', 'text_domain' );
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'facebook' ); ?>"><?php _e( 'Facebook:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'facebook' ); ?>" name="<?php echo $this->get_field_name( 'facebook' ); ?>" type="text" value="<?php echo esc_attr( $facebook ); ?>">
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'twitter' ); ?>"><?php _e( 'Twitter:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'twitter' ); ?>" name="<?php echo $this->get_field_name( 'twitter' ); ?>" type="text" value="<?php echo esc_attr( $twitter ); ?>">
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'instagram' ); ?>"><?php _e( 'Instagram:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'instagram' ); ?>" name="<?php echo $this->get_field_name( 'instagram' ); ?>" type="text" value="<?php echo esc_attr( $instagram ); ?>">
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'linkedin' ); ?>"><?php _e( 'linkedin:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'linkedin' ); ?>" name="<?php echo $this->get_field_name( 'linkedin' ); ?>" type="text" value="<?php echo esc_attr( $linkedin ); ?>">
+        </p>
+        <?php
+
+
+    }
+    
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        $instance['facebook'] = ( ! empty( $new_instance['facebook'] ) ) ? strip_tags( $new_instance['facebook'] ) : '';
+        $instance['twitter'] = ( ! empty( $new_instance['twitter'] ) ) ? strip_tags( $new_instance['twitter'] ) : '';
+        $instance['instagram'] = ( ! empty( $new_instance['instagram'] ) ) ? strip_tags( $new_instance['instagram'] ) : '';
+        $instance['linkedin'] = ( ! empty( $new_instance['linkedin'] ) ) ? strip_tags( $new_instance['linkedin'] ) : '';
+        return $instance;
+    }
 }
+//créer redirection page admin si on clique sur le lien vide
+    
+
+
+
+
 
